cc50f8c3c6e6d4a3ecb4f39535f3406570c86382
Matteo Paonessa <Lymphatus@users.noreply.github.com>
2025-07-14 11:36:50 +0200

    Logging to console and file

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo): void {
+    console.error(error, info.componentStack, React.captureOwnerStack());
+  }
+
+  render(): React.ReactNode {
+    if (this.state.hasError) {
+      return <div className="flex h-screen w-screen items-center justify-center">An error has occurred :(</div>;
+    }
+
+    return this.props.children;
+  }
+}
